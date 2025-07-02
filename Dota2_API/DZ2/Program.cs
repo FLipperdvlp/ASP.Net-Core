25b63a2761cbc1e2d3b56c497db9e41dd1803a5d
Gleb Renkas <102474465+FLipperdvlp@users.noreply.github.com>
2025-07-02 21:48:33 +0300

    Update code
    made code more readable

--- Dota2_API/DZ2/Program.cs
+++ Dota2_API/DZ2/Program.cs
@@ -21,11 +21,11 @@ void GetHeroesHandler(HttpContext context)
 
     if (!string.IsNullOrEmpty(health)) 
     {
-        int health_ = int.Parse(health);
+        int healthInt = int.Parse(health);
         if (healthLimit == "more")   
-            heroes = heroes.Where(hero => hero.Health > health_).ToList();
+            heroes = heroes.Where(hero => hero.Health > healthInt).ToList();
         else if (healthLimit == "less")   
-            heroes = heroes.Where(hero => hero.Health < health_).ToList();  
+            heroes = heroes.Where(hero => hero.Health < healthInt).ToList();  
     }
     else if (!string.IsNullOrEmpty(name)) 
         heroes = heroes.Where(hero => hero.Name.Contains(name, StringComparison.CurrentCultureIgnoreCase)).ToList();
@@ -47,33 +47,55 @@ async Task CreateHeroHandler(HttpContext context)
     HeroesDataBase.Heroes.Add(hero);
     
     context.Response.StatusCode = 201;
-    context.Response.WriteAsJsonAsync(hero);
+    await context.Response.WriteAsJsonAsync(hero);
     // Console.WriteLine($"Hero: {hero.Name} {hero.Health}");
 }
 //          Get /heroes/
 void GetHeroHandler(HttpContext context, string name)
 {
     var hero = HeroesDataBase.Heroes.FirstOrDefault(hero => hero.Name == name);
+    if (hero == null)
+    {
+        context.Response.StatusCode = 404;
+        context.Response.WriteAsync("Here not found");
+    }
     context.Response.WriteAsJsonAsync(hero);
 }
 
 async Task UpdateHeroHandler(HttpContext context, string name)
 {
-    var hero_before = HeroesDataBase.Heroes.FirstOrDefault(hero => hero.Name == name);
-    var hero_after = await context.Request.ReadFromJsonAsync<Hero>();
+    var heroBefore = HeroesDataBase.Heroes.FirstOrDefault(hero => hero.Name == name);
     
-    hero_before.Name = hero_after.Name;
-    hero_before.Attribute = hero_after.Attribute;
-    hero_before.IsMelee = hero_after.IsMelee;
-    hero_before.Health = hero_after.Health;
-    hero_before.Mana = hero_after.Mana;
+    if( heroBefore == null ){ 
+        context.Response.StatusCode = 404;
+        await context.Response.WriteAsync("Hero not found");
+    }
+    var heroAfter = await context.Request.ReadFromJsonAsync<Hero>();
 
-    context.Response.WriteAsJsonAsync(hero_before);
+    if (heroAfter == null)
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsJsonAsync("Invalid hero data");
+    }
+    
+    heroBefore.Name = heroAfter.Name;
+    heroBefore.Attribute = heroAfter.Attribute;
+    heroBefore.IsMelee = heroAfter.IsMelee;
+    heroBefore.Health = heroAfter.Health;
+    heroBefore.Mana = heroAfter.Mana;
+
+    await context.Response.WriteAsJsonAsync(heroBefore);
 }
 
 void DeleteHeroHandler(HttpContext context, string name)
 {
     var hero = HeroesDataBase.Heroes.FirstOrDefault(hero => hero.Name == name);
+
+    if (hero == null)
+    {
+        context.Response.StatusCode = 404;
+        context.Response.WriteAsync("Hero not found");
+    }
     
     HeroesDataBase.Heroes.Remove(hero);
     context.Response.StatusCode = 204;
